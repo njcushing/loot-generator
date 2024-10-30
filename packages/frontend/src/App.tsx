3eebeefd129df1aa57f9470b1fa7f87976fd38f9
njcushing <129661299+njcushing@users.noreply.github.com>
2024-10-30 23:31:25 +0000

    Set window inner width and height as global style rules
    I do it this way to provide an alternative to '100%' in CSS as it accounts for things like the browser search bar occupying screen space on mobile devices that don't seem to be considered when using '100%'.

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
 import { Pages } from "./pages";
 
 export function App() {
+    // Calculate 'vw' and 'vh' units
+    useEffect(() => {
+        const setUnits = () => {
+            const vw = window.innerWidth * 0.01;
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty("--vw", `${vw}px`);
+            document.documentElement.style.setProperty("--vh", `${vh}px`);
+        };
+
+        setUnits();
+        window.addEventListener("resize", setUnits);
+
+        return () => {
+            window.removeEventListener("resize", setUnits);
+        };
+    }, []);
+
     return <Pages.LootGenerator />;
 }
