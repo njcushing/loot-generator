cf499793632a4feb542ac39e596c4f5ca4103fdd
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-02 17:28:18 +0000

    Switch selected quantity state by clicking a quantity option button

--- packages/frontend/src/features/Generate/index.tsx
+++ packages/frontend/src/features/Generate/index.tsx
@@ -1,37 +1,66 @@
+import { useState, useCallback } from "react";
 import styles from "./index.module.css";
 
-const createButton = (text: string, className: string = "generation-quantity-button") => {
-    return (
-        <button
-            // eslint-disable-next-line react/button-has-type
-            type="button"
-            className={styles[className]}
-            onClick={(e) => {
-                e.currentTarget.blur();
-            }}
-            onMouseLeave={(e) => {
-                e.currentTarget.blur();
-            }}
-        >
-            {text}
-        </button>
-    );
-};
+type QuantityOption = { text: string; value: number };
 
 export function Generate() {
+    const [quantitySelected, setQuantitySelected] = useState<number>(0);
+    const [quantityOptions, setQuantityOptions] = useState<QuantityOption[]>([
+        { text: "1", value: 1 },
+        { text: "10", value: 10 },
+        { text: "100", value: 100 },
+        { text: "1000", value: 1000 },
+        { text: "Custom", value: 50 },
+    ]);
+
+    const createButton = useCallback(
+        (text: string, onClickHandler: () => void, className: string) => {
+            return (
+                <button
+                    type="button"
+                    className={styles[className]}
+                    onClick={(e) => {
+                        onClickHandler();
+                        e.currentTarget.blur();
+                    }}
+                    onMouseLeave={(e) => {
+                        e.currentTarget.blur();
+                    }}
+                >
+                    {text}
+                </button>
+            );
+        },
+        [],
+    );
+
     return (
         <div className={styles["generate"]}>
             <div></div>
             <div className={styles["generation-quantity-buttons"]}>
-                {createButton("1")}
-                {createButton("10")}
-                {createButton("100")}
-                {createButton("1000")}
-                {createButton("Custom")}
+                {quantityOptions.map((option, i) => {
+                    const { text, value } = option;
+                    return (
+                        <button
+                            type="button"
+                            className={`${styles["generation-quantity-button"]} ${styles[quantitySelected === i ? "selected" : ""]}`}
+                            onClick={(e) => {
+                                setQuantitySelected(i);
+                                e.currentTarget.blur();
+                            }}
+                            onMouseLeave={(e) => {
+                                e.currentTarget.blur();
+                            }}
+                            key={`generation-quantity-button-${value}`}
+                        >
+                            {text}
+                        </button>
+                    );
+                })}
             </div>
             <div className={styles["generate-and-reset-buttons"]}>
-                {createButton("Generate", "generate-button")}
-                {createButton("Reset", "reset-button")}
+                {createButton("Generate", () => {}, "generate-button")}
+                {createButton("Reset", () => {}, "reset-button")}
             </div>
         </div>
     );
