0476acbeb951a1bd16c73afb36a2d89f910b011a
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-02 20:18:35 +0000

    Alternate background colour of items instead of displaying separator

--- packages/frontend/src/features/Generate/components/Loot/index.tsx
+++ packages/frontend/src/features/Generate/components/Loot/index.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from "react/jsx-runtime";
 import { mockData } from "./utils/mockData";
 import styles from "./index.module.css";
 
@@ -18,17 +17,15 @@ export function Loot() {
             {Object.keys(mockData).map((key, i) => {
                 const item = mockData[key];
                 return (
-                    <Fragment key={key}>
-                        <li className={styles["item"]}>
-                            <p className={`${styles["item-name"]} truncate-ellipsis`}>
-                                {item.name || key}
-                            </p>
-                            <p className={styles["item-quantity"]}>{item.quantity}</p>
-                        </li>
-                        {i < Object.keys(mockData).length - 1 && (
-                            <div className={styles["separator"]}></div>
-                        )}
-                    </Fragment>
+                    <li
+                        className={`${styles["item"]} ${styles[i % 2 === 0 ? "even" : "odd"]}`}
+                        key={key}
+                    >
+                        <p className={`${styles["item-name"]} truncate-ellipsis`}>
+                            {item.name || key}
+                        </p>
+                        <p className={styles["item-quantity"]}>{item.quantity}</p>
+                    </li>
                 );
             })}
         </ul>
