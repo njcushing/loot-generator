1eb6bd1181df6e8bcca415da784bd77a93c8438b
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-02 19:29:19 +0000

    Display item names and quantities in list using mock data

--- packages/frontend/src/features/Generate/components/Loot/index.tsx
+++ packages/frontend/src/features/Generate/components/Loot/index.tsx
@@ -1,5 +1,34 @@
+import { Fragment } from "react/jsx-runtime";
+import { mockData } from "./utils/mockData";
 import styles from "./index.module.css";
 
+export type Item = {
+    name?: string;
+    quantity: number;
+    value?: number;
+};
+
+export type Items = {
+    [key: string]: Item;
+};
+
 export function Loot() {
-    return <div className={styles["loot"]}></div>;
+    return (
+        <ul className={styles["loot"]}>
+            {Object.keys(mockData).map((key, i) => {
+                const item = mockData[key];
+                return (
+                    <Fragment key={key}>
+                        <li className={styles["item"]}>
+                            <p className={styles["item-name"]}>{item.name || key}</p>
+                            <p className={styles["item-quantity"]}>{item.quantity}</p>
+                        </li>
+                        {i < Object.keys(mockData).length - 1 && (
+                            <div className={styles["separator"]}></div>
+                        )}
+                    </Fragment>
+                );
+            })}
+        </ul>
+    );
 }
