17935779be5a0e2563d82795c5d36cc6c8286bf6
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-02 16:59:40 +0000

    Add buttons for various generation quantities

--- packages/frontend/src/features/Generate/index.tsx
+++ packages/frontend/src/features/Generate/index.tsx
@@ -1,5 +1,33 @@
 import styles from "./index.module.css";
 
+const createButton = (text: string) => {
+    return (
+        <button
+            // eslint-disable-next-line react/button-has-type
+            type="button"
+            className={styles["generation-quantity-button"]}
+            onClick={(e) => {
+                e.currentTarget.blur();
+            }}
+            onMouseLeave={(e) => {
+                e.currentTarget.blur();
+            }}
+        >
+            {text}
+        </button>
+    );
+};
+
 export function Generate() {
-    return <div className={styles["generate"]}></div>;
+    return (
+        <div className={styles["generate"]}>
+            <div className={styles["generation-quantity-buttons"]}>
+                {createButton("1")}
+                {createButton("10")}
+                {createButton("100")}
+                {createButton("1000")}
+                {createButton("Custom")}
+            </div>
+        </div>
+    );
 }
