35a67ee6aa8f59121ac8c7c14114d07205946200
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-02 12:14:44 +0000

    Create basic structure for LootGenerator page component

--- packages/frontend/src/pages/LootGenerator/index.tsx
+++ packages/frontend/src/pages/LootGenerator/index.tsx
@@ -1,3 +1,42 @@
+import { useState, useEffect, useRef } from "react";
+import useResizeObserverElement from "@/hooks/useResizeObserverElement";
+import { Structural } from "@/components/structural";
+import { version } from "../../../package.json";
+import styles from "./index.module.css";
+
 export function LootGenerator() {
-    return <div></div>;
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [containerSize] = useResizeObserverElement({ ref: containerRef });
+    const [layout, setLayout] = useState<"wide" | "thin">("wide");
+
+    useEffect(() => {
+        if (containerSize[0] >= 800) setLayout("wide");
+        else setLayout("thin");
+    }, [containerSize]);
+
+    return (
+        <div className={`${styles["page"]} ${styles[`${layout}`]}`} ref={containerRef}>
+            <div className={styles["left-panel"]}>
+                <h1 className={styles["title"]}>Loot Generator</h1>
+                <p className={styles["name"]}>by njcushing</p>
+                <p className={styles["version"]}>{`v${version}`}</p>
+                <Structural.TabSelector
+                    tabs={{
+                        design: { name: "Design", content: <p>Design</p>, position: "left" },
+                        code: { name: "Code", content: <p>Code</p>, position: "left" },
+                        about: { name: "About", content: <p>About</p>, position: "right" },
+                    }}
+                />
+            </div>
+            <div className={styles["right-panel"]}>
+                <Structural.TabSelector
+                    tabs={{
+                        generate: { name: "Generate", content: <p>Generate</p>, position: "left" },
+                        code: { name: "Code", content: <p>Code</p>, position: "left" },
+                        data: { name: "Data", content: <p>Data</p>, position: "left" },
+                    }}
+                />
+            </div>
+        </div>
+    );
 }
