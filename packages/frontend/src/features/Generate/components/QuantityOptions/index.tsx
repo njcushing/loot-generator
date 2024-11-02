600e05f0037ae3980a9bb0fd4d2d9ece7ee780f2
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-02 17:55:59 +0000

    Use LootGenerator context within QuantityOptions component

--- packages/frontend/src/features/Generate/components/QuantityOptions/index.tsx
+++ packages/frontend/src/features/Generate/components/QuantityOptions/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useMemo } from "react";
+import { LootGeneratorContext } from "@/pages/LootGenerator";
 import styles from "./index.module.css";
 
 type QuantityOption = { text: string; value: number };
@@ -8,14 +9,20 @@ export type TQuantityOptions = {
 };
 
 export function QuantityOptions({ newQuantitySelected }: TQuantityOptions) {
-    const [quantityOptionSelected, setQuantityOptionSelected] = useState<number>(0);
-    const [quantityOptions, setQuantityOptions] = useState<QuantityOption[]>([
-        { text: "1", value: 1 },
-        { text: "10", value: 10 },
-        { text: "100", value: 100 },
-        { text: "1000", value: 1000 },
-        { text: "Custom", value: 50 },
-    ]);
+    const { lootGeneratorState, setLootGeneratorState } = useContext(LootGeneratorContext);
+
+    const quantityOptionSelected = useMemo<number>(() => {
+        return lootGeneratorState.quantityOptionSelected;
+    }, [lootGeneratorState.quantityOptionSelected]);
+    const quantityOptions = useMemo<QuantityOption[]>(() => {
+        return [
+            { text: "1", value: 1 },
+            { text: "10", value: 10 },
+            { text: "100", value: 100 },
+            { text: "1000", value: 1000 },
+            { text: "Custom", value: lootGeneratorState.customQuantity },
+        ];
+    }, [lootGeneratorState.customQuantity]);
 
     useEffect(() => {
         newQuantitySelected(quantityOptions[quantityOptionSelected].value);
@@ -30,7 +37,10 @@ export function QuantityOptions({ newQuantitySelected }: TQuantityOptions) {
                         type="button"
                         className={`${styles["generation-quantity-button"]} ${styles[quantityOptionSelected === i ? "selected" : ""]}`}
                         onClick={(e) => {
-                            setQuantityOptionSelected(i);
+                            setLootGeneratorState({
+                                ...lootGeneratorState,
+                                quantityOptionSelected: i,
+                            });
                             e.currentTarget.blur();
                         }}
                         onMouseLeave={(e) => {
