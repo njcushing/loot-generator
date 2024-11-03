7dacced3755c01b72cd8e87d5df8fd78db314b54
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-03 16:43:19 +0000

    Create sort options' fields using a function to reduce code repitition

--- packages/frontend/src/features/Generate/components/SortOptions/index.tsx
+++ packages/frontend/src/features/Generate/components/SortOptions/index.tsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useState, useEffect, useCallback } from "react";
 import { LootGeneratorContext } from "@/pages/LootGenerator";
-import { SortOrder } from "@/utils/sortLoot/sortLoot";
+import { SortCriteria, SortOrder } from "@/utils/sortLoot/sortLoot";
 import styles from "./index.module.css";
 
 export function SortOptions() {
@@ -14,89 +14,54 @@ export function SortOptions() {
         setLootGeneratorStateProperty("sortOptions", sortOptions);
     }, [setLootGeneratorStateProperty, sortOptions]);
 
-    const nameInputRef = useRef<HTMLInputElement>(null);
-    const quantityInputRef = useRef<HTMLInputElement>(null);
+    const createSortOptionField = useCallback(
+        (criteria: SortCriteria, label: string) => {
+            return (
+                <fieldset>
+                    <label htmlFor={`sort-${criteria}`}>
+                        <input
+                            type="checkbox"
+                            id={`sort-${criteria}`}
+                            checked={lootGeneratorState.sortOptions.has(criteria)}
+                            onChange={(e) => {
+                                setSortOptions(() => {
+                                    if (e.target.checked) return new Map([[criteria, "ascending"]]);
+                                    return new Map();
+                                });
+                            }}
+                        />
+                        {label}
+                    </label>
+                    {lootGeneratorState.sortOptions.has(criteria) && (
+                        <select
+                            id={`sort-${criteria}-order`}
+                            name={`sort-${criteria}-order`}
+                            defaultValue={
+                                lootGeneratorState.sortOptions.get(criteria) || "ascending"
+                            }
+                            onChange={(e) => {
+                                setSortOptions((currentSortOptions) => {
+                                    const opts = new Map(currentSortOptions);
+                                    if (opts.has(criteria))
+                                        opts.set(criteria, e.target.value as SortOrder);
+                                    return opts;
+                                });
+                            }}
+                        >
+                            <option value="ascending">Ascending</option>
+                            <option value="descending">Descending</option>
+                        </select>
+                    )}
+                </fieldset>
+            );
+        },
+        [lootGeneratorState.sortOptions],
+    );
 
     return (
         <form className={styles["sort-options"]}>
-            <fieldset>
-                <label htmlFor="sort-name">
-                    <input
-                        type="checkbox"
-                        id="sort-name"
-                        checked={lootGeneratorState.sortOptions.has("name")}
-                        onChange={(e) => {
-                            setSortOptions((currentSortOptions) => {
-                                const opts = new Map(currentSortOptions);
-                                if (e.target.checked) {
-                                    if (!opts.has("name")) opts.set("name", "ascending");
-                                    if (opts.has("quantity")) opts.delete("quantity");
-                                } else if (opts.has("name")) opts.delete("name");
-                                return opts;
-                            });
-                        }}
-                        ref={nameInputRef}
-                    />
-                    Name
-                </label>
-                {lootGeneratorState.sortOptions.has("name") && (
-                    <select
-                        id="sort-name-direction"
-                        name="sort-name-direction"
-                        defaultValue={lootGeneratorState.sortOptions.get("name") || "ascending"}
-                        onChange={(e) => {
-                            setSortOptions((currentSortOptions) => {
-                                const opts = new Map(currentSortOptions);
-                                if (opts.has("name")) opts.set("name", e.target.value as SortOrder);
-                                return opts;
-                            });
-                        }}
-                    >
-                        <option value="ascending">Ascending</option>
-                        <option value="descending">Descending</option>
-                    </select>
-                )}
-            </fieldset>
-            <fieldset>
-                <label htmlFor="sort-quantity">
-                    <input
-                        type="checkbox"
-                        id="sort-quantity"
-                        checked={lootGeneratorState.sortOptions.has("quantity")}
-                        onChange={(e) => {
-                            setSortOptions((currentSortOptions) => {
-                                const opts = new Map(currentSortOptions);
-                                if (e.target.checked) {
-                                    if (!opts.has("quantity")) opts.set("quantity", "ascending");
-                                    if (opts.has("name")) opts.delete("name");
-                                } else if (opts.has("quantity")) opts.delete("quantity");
-                                return opts;
-                            });
-                        }}
-                        ref={quantityInputRef}
-                    />
-                    Quantity
-                </label>
-                {lootGeneratorState.sortOptions.has("quantity") && (
-                    <select
-                        id="sort-quantity-direction"
-                        name="sort-quantity-direction"
-                        defaultValue={lootGeneratorState.sortOptions.get("quantity") || "ascending"}
-                        onChange={(e) => {
-                            setSortOptions((currentSortOptions) => {
-                                const opts = new Map(currentSortOptions);
-                                if (opts.has("quantity")) {
-                                    opts.set("quantity", e.target.value as SortOrder);
-                                }
-                                return opts;
-                            });
-                        }}
-                    >
-                        <option value="ascending">Ascending</option>
-                        <option value="descending">Descending</option>
-                    </select>
-                )}
-            </fieldset>
+            {createSortOptionField("name", "Name")}
+            {createSortOptionField("quantity", "Quantity")}
         </form>
     );
 }
