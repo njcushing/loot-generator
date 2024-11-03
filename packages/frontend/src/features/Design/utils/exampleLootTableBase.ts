540a2186460876fef6fc137925761b5a76570b58
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-03 20:27:28 +0000

    Create an example loot table base for testing

---
+++ packages/frontend/src/features/Design/utils/exampleLootTableBase.ts
@@ -0,0 +1,20 @@
+import { LootTableBase } from "@/utils/types";
+import { createLootItem, createLootTable } from "@/utils/generateLoot";
+
+export const exampleLootTableBase: LootTableBase = {
+    loot: [
+        createLootItem({ information: { name: "Apple" }, weight: 10 }),
+        createLootItem({ information: { name: "Banana" }, weight: 10 }),
+        createLootItem({ information: { name: "Orange" }, weight: 10 }),
+        createLootItem({ information: { name: "Peach" }, weight: 10 }),
+        createLootItem({ information: { name: "Cherry" }, weight: 10 }),
+        createLootTable({
+            weight: 100,
+            loot: [
+                createLootItem({ information: { name: "Pineapple" }, weight: 10 }),
+                createLootItem({ information: { name: "Kiwi" }, weight: 10 }),
+                createLootItem({ information: { name: "Watermelon" }, weight: 10 }),
+            ],
+        }),
+    ],
+};
