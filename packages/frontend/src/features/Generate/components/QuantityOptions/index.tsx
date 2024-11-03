1a8362404c9fb3d0e4eeb2a635f8655b87a638cb
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-03 16:32:32 +0000

    Add input for setting custom generation quantity value

--- packages/frontend/src/features/Generate/components/QuantityOptions/index.tsx
+++ packages/frontend/src/features/Generate/components/QuantityOptions/index.tsx
@@ -29,26 +29,44 @@ export function QuantityOptions({ newQuantitySelected }: TQuantityOptions) {
     }, [newQuantitySelected, quantityOptionSelected, quantityOptions]);
 
     return (
-        <div className={styles["generation-quantity-buttons"]}>
-            {quantityOptions.map((option, i) => {
-                const { text, value } = option;
-                return (
-                    <button
-                        type="button"
-                        className={`${styles["generation-quantity-button"]} ${styles[quantityOptionSelected === i ? "selected" : ""]}`}
-                        onClick={(e) => {
-                            setLootGeneratorStateProperty("quantityOptionSelected", i);
-                            e.currentTarget.blur();
+        <div className={styles["generation-quantity-options"]}>
+            <div className={styles["quantity-buttons"]}>
+                {quantityOptions.map((option, i) => {
+                    const { text } = option;
+                    return (
+                        <button
+                            type="button"
+                            className={`${styles["generation-quantity-button"]} ${styles[quantityOptionSelected === i ? "selected" : ""]}`}
+                            onClick={(e) => {
+                                setLootGeneratorStateProperty("quantityOptionSelected", i);
+                                e.currentTarget.blur();
+                            }}
+                            onMouseLeave={(e) => {
+                                e.currentTarget.blur();
+                            }}
+                            key={`generation-quantity-button-${text}`}
+                        >
+                            {text}
+                        </button>
+                    );
+                })}
+            </div>
+            {quantityOptionSelected === 4 && (
+                <label htmlFor="custom-quantity" className={styles["custom-quantity-field"]}>
+                    Enter custom quantity:
+                    <input
+                        type="number"
+                        value={lootGeneratorState.customQuantity}
+                        onChange={(e) => {
+                            setLootGeneratorStateProperty(
+                                "customQuantity",
+                                Math.max(1, Number(e.target.value)),
+                            );
                         }}
-                        onMouseLeave={(e) => {
-                            e.currentTarget.blur();
-                        }}
-                        key={`generation-quantity-button-${value}`}
-                    >
-                        {text}
-                    </button>
-                );
-            })}
+                        id="custom-quantity"
+                    />
+                </label>
+            )}
         </div>
     );
 }
