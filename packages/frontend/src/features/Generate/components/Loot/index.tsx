b31c8a3875847107cfe92c749625c07ad5306341
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-03 17:19:49 +0000

    Delete mock loot as it is no longer needed
    I have also set the loot being displayed in the Loot component to that from the LootGenerator component's context.

--- packages/frontend/src/features/Generate/components/Loot/index.tsx
+++ packages/frontend/src/features/Generate/components/Loot/index.tsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { LootGeneratorContext } from "@/pages/LootGenerator";
 import { sortLoot } from "@/utils/sortLoot/sortLoot";
-import { mockData } from "./utils/mockData";
 import styles from "./index.module.css";
 
 export function Loot() {
@@ -9,22 +8,24 @@ export function Loot() {
 
     return (
         <ul className={styles["loot"]}>
-            {[...sortLoot(mockData, lootGeneratorState.sortOptions).keys()].map((key, i) => {
-                const item = mockData.get(key);
-                return (
-                    item && (
-                        <li
-                            className={`${styles["item"]} ${styles[i % 2 === 0 ? "even" : "odd"]}`}
-                            key={key}
-                        >
-                            <p className={`${styles["item-name"]} truncate-ellipsis`}>
-                                {item.name || key}
-                            </p>
-                            <p className={styles["item-quantity"]}>{item.quantity}</p>
-                        </li>
-                    )
-                );
-            })}
+            {[...sortLoot(lootGeneratorState.loot, lootGeneratorState.sortOptions).keys()].map(
+                (key, i) => {
+                    const item = lootGeneratorState.loot.get(key);
+                    return (
+                        item && (
+                            <li
+                                className={`${styles["item"]} ${styles[i % 2 === 0 ? "even" : "odd"]}`}
+                                key={key}
+                            >
+                                <p className={`${styles["item-name"]} truncate-ellipsis`}>
+                                    {item.name || key}
+                                </p>
+                                <p className={styles["item-quantity"]}>{item.quantity}</p>
+                            </li>
+                        )
+                    );
+                },
+            )}
         </ul>
     );
 }
--- packages/frontend/src/features/Generate/components/Loot/utils/mockData.ts
+++
@@ -1,9 +0,0 @@
-import { Loot } from "@/utils/types";
-
-export const mockData: Loot = new Map([
-    ["apple", { name: "Apple", quantity: 5 }],
-    ["banana", { name: "Banana", quantity: 8 }],
-    ["orange", { name: "Orange", quantity: 14 }],
-    ["cherry", { name: "Cherry", quantity: 6 }],
-    ["peach", { name: "Peach", quantity: 2 }],
-]);
