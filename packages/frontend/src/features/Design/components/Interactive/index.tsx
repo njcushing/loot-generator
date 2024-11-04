c5a56088383f0c17a4ddceaf0a11a434d03ddc30
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-04 21:51:50 +0000

    Store 'collapsed'/'expanded' state of each item and table menu in state

--- packages/frontend/src/features/Design/components/Interactive/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/index.tsx
@@ -1,10 +1,44 @@
-import { useContext, useCallback } from "react";
+import { useContext, useState, useCallback, useEffect } from "react";
 import { LootGeneratorContext } from "@/pages/LootGenerator";
 import { LootItem, LootTable } from "@/utils/types";
 import styles from "./index.module.css";
 
+type MenuStates = Map<string, "collapsed" | "expanded">;
+
+const updateStates = (
+    entry: LootTable | LootItem,
+    previousStates: MenuStates,
+    newStates: MenuStates,
+): MenuStates => {
+    let mutableNewStates = new Map(newStates);
+
+    const defaultState = entry.type === "item" ? "collapsed" : "expanded";
+    mutableNewStates.set(
+        entry.key,
+        !previousStates.has(entry.key) ? defaultState : previousStates.get(entry.key)!,
+    );
+
+    if (entry.type === "table") {
+        [...entry.loot.keys()].forEach((key) => {
+            const subEntry = entry.loot[key];
+            mutableNewStates = updateStates(subEntry, previousStates, mutableNewStates);
+        });
+    }
+
+    return mutableNewStates;
+};
+
 export function Interactive() {
     const { lootGeneratorState } = useContext(LootGeneratorContext);
+
+    const [menuStates, setMenuStates] = useState<Map<string, "collapsed" | "expanded">>(() => {
+        return updateStates(lootGeneratorState.lootTable, new Map(), new Map());
+    });
+    useEffect(() => {
+        setMenuStates((currentMenuStates) => {
+            return updateStates(lootGeneratorState.lootTable, currentMenuStates, new Map());
+        });
+    }, [lootGeneratorState.lootTable]);
 
     const createItemField = useCallback((entry: LootItem) => {
         return (
