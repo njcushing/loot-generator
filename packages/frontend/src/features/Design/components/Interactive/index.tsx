6e552c25aeae5f208e0e15cd6fb07c0566d60a31
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-04 22:03:03 +0000

    Toggle menu state when button is clicked
    The symbol is also changed to represent the current state of the menu (a plus symbol when the menu is collapsed, and a minus symbol when the menu is expanded).

--- packages/frontend/src/features/Design/components/Interactive/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/index.tsx
@@ -40,56 +40,76 @@ export function Interactive() {
         });
     }, [lootGeneratorState.lootTable]);
 
-    const createItemField = useCallback((entry: LootItem) => {
-        return (
-            <div className={styles["item"]} key={entry.key}>
-                <button
-                    type="button"
-                    className={styles["expand-collapse-button"]}
-                    onClick={(e) => {
-                        e.currentTarget.blur();
-                    }}
-                    onMouseLeave={(e) => {
-                        e.currentTarget.blur();
-                    }}
-                >
-                    <p className={styles["symbol"]}>+</p>
-                    <p className={styles["item-name"]}>
-                        {entry.information.name || "Unnamed Item"}
-                    </p>
-                </button>
-            </div>
-        );
+    const toggleMenuState = useCallback((key: string) => {
+        setMenuStates((currentMenuStates) => {
+            const newMenuStates = new Map(currentMenuStates);
+            const currentState = newMenuStates.get(key);
+            newMenuStates.set(key, currentState === "collapsed" ? "expanded" : "collapsed");
+            return newMenuStates;
+        });
     }, []);
 
-    const createTableField = useCallback(
-        (entry: LootTable) => {
+    const createItemField = useCallback(
+        (entry: LootItem) => {
+            const { key } = entry;
+            const menuState = menuStates.get(key);
             return (
-                <div className={styles["table"]} key={entry.key}>
+                <div className={styles["item"]} key={key}>
                     <button
                         type="button"
                         className={styles["expand-collapse-button"]}
                         onClick={(e) => {
+                            toggleMenuState(key);
                             e.currentTarget.blur();
                         }}
                         onMouseLeave={(e) => {
                             e.currentTarget.blur();
                         }}
                     >
-                        <p className={styles["symbol"]}>+</p>
-                        <p className={styles["table-name"]}>{entry.name || "Unnamed Table"}</p>
+                        <p className={styles["symbol"]}>{menuState === "collapsed" ? "+" : "-"}</p>
+                        <p className={styles["item-name"]}>
+                            {entry.information.name || "Unnamed Item"}
+                        </p>
                     </button>
-                    <div className={styles["table-entries"]}>
-                        {entry.loot.map((subEntry) => {
-                            if (subEntry.type === "item") return createItemField(subEntry);
-                            if (subEntry.type === "table") return createTableField(subEntry);
-                            return null;
-                        })}
-                    </div>
                 </div>
             );
         },
-        [createItemField],
+        [menuStates, toggleMenuState],
+    );
+
+    const createTableField = useCallback(
+        (entry: LootTable) => {
+            const { key } = entry;
+            const menuState = menuStates.get(key);
+            return (
+                <div className={styles["table"]} key={key}>
+                    <button
+                        type="button"
+                        className={styles["expand-collapse-button"]}
+                        onClick={(e) => {
+                            toggleMenuState(key);
+                            e.currentTarget.blur();
+                        }}
+                        onMouseLeave={(e) => {
+                            e.currentTarget.blur();
+                        }}
+                    >
+                        <p className={styles["symbol"]}>{menuState === "collapsed" ? "+" : "-"}</p>
+                        <p className={styles["table-name"]}>{entry.name || "Unnamed Table"}</p>
+                    </button>
+                    {menuState === "expanded" && (
+                        <div className={styles["table-entries"]}>
+                            {entry.loot.map((subEntry) => {
+                                if (subEntry.type === "item") return createItemField(subEntry);
+                                if (subEntry.type === "table") return createTableField(subEntry);
+                                return null;
+                            })}
+                        </div>
+                    )}
+                </div>
+            );
+        },
+        [menuStates, createItemField, toggleMenuState],
     );
 
     return (
