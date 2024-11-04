5093567fcde41ccf3debdc30fa521d61e2ca9908
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-04 20:31:22 +0000

    Display an 'item' button for each item in the base loot table

--- packages/frontend/src/features/Design/components/Interactive/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/index.tsx
@@ -1,5 +1,40 @@
+import { useContext, useCallback } from "react";
+import { LootGeneratorContext } from "@/pages/LootGenerator";
+import { LootItem } from "@/utils/types";
+import { v4 as uuid } from "uuid";
 import styles from "./index.module.css";
 
 export function Interactive() {
-    return <div className={styles["interactive"]}></div>;
+    const { lootGeneratorState } = useContext(LootGeneratorContext);
+
+    const createItemField = useCallback((entry: LootItem) => {
+        return (
+            <div className={styles["item"]} key={uuid()}>
+                <button
+                    type="button"
+                    className={styles["expand-collapse-button"]}
+                    onClick={(e) => {
+                        e.currentTarget.blur();
+                    }}
+                    onMouseLeave={(e) => {
+                        e.currentTarget.blur();
+                    }}
+                >
+                    <p className={styles["symbol"]}>+</p>
+                    <p className={styles["item-name"]}>
+                        {entry.information.name || "Unnamed Item"}
+                    </p>
+                </button>
+            </div>
+        );
+    }, []);
+
+    return (
+        <div className={styles["interactive"]}>
+            {lootGeneratorState.lootTable.loot.map((entry) => {
+                if (entry.type === "item") return createItemField(entry);
+                return null;
+            })}
+        </div>
+    );
 }
