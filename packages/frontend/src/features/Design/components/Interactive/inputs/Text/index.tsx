50501b6f23b895e8e3a820cdd3eea2cb97b45782
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-09 14:58:48 +0000

    Create a component for displaying and mutating entries' text fields

---
+++ packages/frontend/src/features/Design/components/Interactive/inputs/Text/index.tsx
@@ -0,0 +1,39 @@
+import { useContext, useCallback } from "react";
+import { LootGeneratorContext } from "@/pages/LootGenerator";
+import { LootItem, LootTable } from "@/utils/types";
+import { findNestedEntry, mutateNestedField } from "../../utils/mutateNestedEntry";
+import "./index.module.css";
+
+export type TText = {
+    entryKey: (LootItem | LootTable)["key"];
+    labelText: string;
+    defaultValue: string;
+    fieldPath: string[];
+};
+
+export function Text({ entryKey, labelText, defaultValue, fieldPath }: TText) {
+    const { lootGeneratorState, setLootGeneratorStateProperty } = useContext(LootGeneratorContext);
+
+    const editEntry = useCallback(
+        (value: unknown) => {
+            const copy: LootTable = JSON.parse(JSON.stringify(lootGeneratorState.lootTable));
+            const entry = findNestedEntry(entryKey, copy);
+            if (!entry) return;
+            mutateNestedField(fieldPath, value, entry);
+            setLootGeneratorStateProperty("lootTable", copy);
+        },
+        [entryKey, fieldPath, lootGeneratorState.lootTable, setLootGeneratorStateProperty],
+    );
+
+    return (
+        <label htmlFor={`${entryKey}-${fieldPath.join()}`}>
+            {labelText}:{" "}
+            <input
+                type="text"
+                id={`${entryKey}-${fieldPath.join()}`}
+                defaultValue={defaultValue}
+                onChange={(e) => editEntry(e.target.value)}
+            ></input>
+        </label>
+    );
+}
