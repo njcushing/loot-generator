cddb13a2110bfb37a1543efaf7c91759db6d5393
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-09 14:12:13 +0000

    Move 'manageStates' function to its own utility function file
    I also renamed it to just 'update'.

---
+++ packages/frontend/src/features/Design/components/Interactive/utils/manageMenuStates.ts
@@ -0,0 +1,26 @@
+import { LootTable, LootItem } from "@/utils/types";
+
+export type MenuStates = Map<string, "collapsed" | "expanded">;
+
+export const update = (
+    entry: LootTable | LootItem,
+    previousStates: MenuStates,
+    newStates: MenuStates,
+): MenuStates => {
+    let mutableNewStates = new Map(newStates);
+
+    const defaultState = entry.type === "item" ? "collapsed" : "expanded";
+    mutableNewStates.set(
+        entry.key,
+        !previousStates.has(entry.key) ? defaultState : previousStates.get(entry.key)!,
+    );
+
+    if (entry.type === "table") {
+        [...entry.loot.keys()].forEach((key) => {
+            const subEntry = entry.loot[key];
+            mutableNewStates = update(subEntry, previousStates, mutableNewStates);
+        });
+    }
+
+    return mutableNewStates;
+};
