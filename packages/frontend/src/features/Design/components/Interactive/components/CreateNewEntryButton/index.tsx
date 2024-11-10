50830c80591834aec9420784d257f8a48af4eb06
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-10 13:29:03 +0000

    Display menu with options upon clicking CreateNewEntryButton

--- packages/frontend/src/features/Design/components/Interactive/components/CreateNewEntryButton/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/components/CreateNewEntryButton/index.tsx
@@ -1,18 +1,52 @@
+import { useState } from "react";
 import styles from "./index.module.css";
 
 export function CreateNewEntryButton() {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
     return (
-        <button
-            type="button"
-            className={`${styles["create-new-entry-button"]} material-symbols-sharp`}
-            onClick={(e) => {
-                e.currentTarget.blur();
-            }}
-            onMouseLeave={(e) => {
-                e.currentTarget.blur();
-            }}
-        >
-            Add_Circle
-        </button>
+        <div className={`${styles["create-new-entry-button-wrapper"]} material-symbols-sharp`}>
+            <button
+                type="button"
+                className={`${styles["create-new-entry-button"]} material-symbols-sharp`}
+                onClick={(e) => {
+                    setMenuOpen(!menuOpen);
+                    e.currentTarget.blur();
+                }}
+                onMouseLeave={(e) => {
+                    e.currentTarget.blur();
+                }}
+            >
+                Add_Circle
+            </button>
+            {menuOpen && (
+                <div className={styles["creation-menu"]}>
+                    <button
+                        type="button"
+                        className={styles["create-new-table-button"]}
+                        onClick={(e) => {
+                            e.currentTarget.blur();
+                        }}
+                        onMouseLeave={(e) => {
+                            e.currentTarget.blur();
+                        }}
+                    >
+                        Create New Table
+                    </button>
+                    <button
+                        type="button"
+                        className={styles["create-new-item-button"]}
+                        onClick={(e) => {
+                            e.currentTarget.blur();
+                        }}
+                        onMouseLeave={(e) => {
+                            e.currentTarget.blur();
+                        }}
+                    >
+                        Create New Item
+                    </button>
+                </div>
+            )}
+        </div>
     );
 }
