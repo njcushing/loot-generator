a1963f96a460c790cab075c31a3e7ccb2eec3204
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-10 12:33:56 +0000

    Move TableEntry to its own component

---
+++ packages/frontend/src/features/Design/components/Interactive/components/TableEntry/index.tsx
@@ -0,0 +1,68 @@
+import { useContext } from "react";
+import { LootTable } from "@/utils/types";
+import { InteractiveContext } from "../..";
+import { ToggleButton } from "../ToggleButton";
+import { CreateNewEntryButton } from "../CreateNewEntryButton";
+import { SaveAsPresetButton } from "../SaveAsPresetButton";
+import { DeleteEntryButton } from "../DeleteEntryButton";
+import { ItemEntry } from "../ItemEntry";
+import { Inputs } from "../../inputs";
+import styles from "./index.module.css";
+
+export type TTableEntry = {
+    entry: LootTable;
+};
+
+export function TableEntry({ entry }: TTableEntry) {
+    const { menuStates } = useContext(InteractiveContext);
+
+    const { key, name, weight } = entry;
+
+    return (
+        <li className={styles["table"]} key={key}>
+            <div className={styles["table-entry-bar"]}>
+                <div className={styles["toggle-button-container"]}>
+                    <ToggleButton entry={entry} />
+                </div>
+                <div className={styles["create-new-entry-button-container"]}>
+                    <CreateNewEntryButton />
+                </div>
+                <div className={styles["save-as-preset-button-container"]}>
+                    <SaveAsPresetButton />
+                </div>
+                <div className={styles["delete-entry-button-container"]}>
+                    <DeleteEntryButton />
+                </div>
+            </div>
+            {menuStates.get(key) === "expanded" && (
+                <>
+                    <div className={styles["table-entry-properties"]}>
+                        <Inputs.Text
+                            entryKey={key}
+                            labelText="Name"
+                            defaultValue={name || ""}
+                            fieldPath={["name"]}
+                        />
+                        <Inputs.Numeric
+                            entryKey={key}
+                            labelText="Weight"
+                            defaultValue={weight || 1}
+                            fieldPath={["weight"]}
+                        />
+                    </div>
+                    <ul className={styles["table-entries"]}>
+                        {entry.loot.map((subEntry) => {
+                            if (subEntry.type === "item") {
+                                return <ItemEntry entry={subEntry} key={subEntry.key} />;
+                            }
+                            if (subEntry.type === "table") {
+                                return <TableEntry entry={subEntry} key={subEntry.key} />;
+                            }
+                            return null;
+                        })}
+                    </ul>
+                </>
+            )}
+        </li>
+    );
+}
--- packages/frontend/src/features/Design/components/Interactive/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/index.tsx
@@ -1,12 +1,6 @@
-import { createContext, useContext, useState, useCallback, useEffect, useMemo } from "react";
+import { createContext, useContext, useState, useEffect, useMemo } from "react";
 import { LootGeneratorContext } from "@/pages/LootGenerator";
-import { LootTable } from "@/utils/types";
-import { ItemEntry } from "./components/ItemEntry";
-import { ToggleButton } from "./components/ToggleButton";
-import { Inputs } from "./inputs";
-import { CreateNewEntryButton } from "./components/CreateNewEntryButton";
-import { SaveAsPresetButton } from "./components/SaveAsPresetButton";
-import { DeleteEntryButton } from "./components/DeleteEntryButton";
+import { TableEntry } from "./components/TableEntry";
 import * as manageMenuStates from "./utils/manageMenuStates";
 import styles from "./index.module.css";
 
@@ -38,66 +32,15 @@ export function Interactive() {
         });
     }, [lootGeneratorState.lootTable]);
 
-    const createTableMenu = useCallback(
-        (entry: LootTable) => {
-            const { key, name, weight } = entry;
-            const menuState = menuStates.get(key);
-            return (
-                <div className={styles["table"]} key={key}>
-                    <div className={styles["table-menu-bar"]}>
-                        <div className={styles["toggle-button-container"]}>
-                            <ToggleButton entry={entry} />
-                        </div>
-                        <div className={styles["create-new-entry-button-container"]}>
-                            <CreateNewEntryButton />
-                        </div>
-                        <div className={styles["save-as-preset-button-container"]}>
-                            <SaveAsPresetButton />
-                        </div>
-                        <div className={styles["delete-entry-button-container"]}>
-                            <DeleteEntryButton />
-                        </div>
-                    </div>
-                    {menuState === "expanded" && (
-                        <>
-                            <div className={styles["table-menu-properties"]}>
-                                <Inputs.Text
-                                    entryKey={key}
-                                    labelText="Name"
-                                    defaultValue={name || ""}
-                                    fieldPath={["name"]}
-                                />
-                                <Inputs.Numeric
-                                    entryKey={key}
-                                    labelText="Weight"
-                                    defaultValue={weight || 1}
-                                    fieldPath={["weight"]}
-                                />
-                            </div>
-                            <ul className={styles["table-entries"]}>
-                                {entry.loot.map((subEntry) => {
-                                    if (subEntry.type === "item") {
-                                        return <ItemEntry entry={subEntry} key={subEntry.key} />;
-                                    }
-                                    if (subEntry.type === "table") return createTableMenu(subEntry);
-                                    return null;
-                                })}
-                            </ul>
-                        </>
-                    )}
-                </div>
-            );
-        },
-        [menuStates],
-    );
-
     return (
         <InteractiveContext.Provider
             value={useMemo(() => ({ menuStates, setMenuStates }), [menuStates, setMenuStates])}
         >
-            <div className={styles["interactive"]}>
-                {lootGeneratorState.lootTable && createTableMenu(lootGeneratorState.lootTable)}
-            </div>
+            <ul className={styles["interactive"]}>
+                {lootGeneratorState.lootTable && (
+                    <TableEntry entry={lootGeneratorState.lootTable} />
+                )}
+            </ul>
         </InteractiveContext.Provider>
     );
 }
