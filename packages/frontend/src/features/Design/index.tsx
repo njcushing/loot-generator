4ac1c291ddcbc80d7c16a1eb3554d28f88b3fb59
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-10 12:04:41 +0000

    Display Presets sub-tab in Design tab

--- packages/frontend/src/features/Design/index.tsx
+++ packages/frontend/src/features/Design/index.tsx
@@ -1,6 +1,7 @@
 import { TabSelector } from "@/components/structural/components/TabSelector";
 import { Interactive } from "./components/Interactive";
 import { JSONDisplay } from "./components/JSONDisplay";
+import { Presets } from "./components/Presets";
 import styles from "./index.module.css";
 
 export function Design() {
@@ -9,6 +10,7 @@ export function Design() {
             tabs={{
                 interactive: { name: "Interactive", content: <Interactive />, position: "left" },
                 JSON: { name: "JSON", content: <JSONDisplay />, position: "left" },
+                presets: { name: "Presets", content: <Presets />, position: "left" },
             }}
         />
     );
