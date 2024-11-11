f3196182dca25f5e2cb3c258663c96b8ce00288f
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-11 20:12:16 +0000

    Write boilerplate for Tables and Items preset tab components

---
+++ packages/frontend/src/features/Design/components/Presets/components/Items/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./index.module.css";
+
+export function Items() {
+    return <div className={styles["preset-items"]}></div>;
+}
---
+++ packages/frontend/src/features/Design/components/Presets/components/Tables/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./index.module.css";
+
+export function Tables() {
+    return <div className={styles["preset-tables"]}></div>;
+}
