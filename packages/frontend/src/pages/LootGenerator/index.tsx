2c699cb16e857dfddb3a8c943791ff001b0016af
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-11 20:54:15 +0000

    Update type for 'presets' field on LootGenerator context

--- packages/frontend/src/pages/LootGenerator/index.tsx
+++ packages/frontend/src/pages/LootGenerator/index.tsx
@@ -2,7 +2,7 @@ import { createContext, useState, useEffect, useRef, useMemo, useCallback } from
 import useResizeObserverElement from "@/hooks/useResizeObserverElement";
 import { Structural } from "@/components/structural";
 import { Generate } from "@/features/Generate";
-import { LootItem, LootTable, Loot, SortOptions } from "@/utils/types";
+import { LootTable, Preset, Loot, SortOptions } from "@/utils/types";
 import { Design } from "@/features/Design";
 import { exampleLootTable } from "@/features/Design/utils/exampleLootTable";
 import { version } from "../../../package.json";
@@ -11,7 +11,7 @@ import styles from "./index.module.css";
 export type LootGeneratorState = {
     loot: Loot;
     lootTable: LootTable;
-    presets: Map<string, LootItem | LootTable>;
+    presets: Map<string, Preset>;
     quantitySelected: number;
     quantityOptionSelected: number;
     customQuantity: number;
