f3f9e29d962dd8fbd031453bb00d487e138cd50d
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-11 20:12:37 +0000

    Display Tables and Items sub-tabs in Presets tab

--- packages/frontend/src/features/Design/components/Presets/index.tsx
+++ packages/frontend/src/features/Design/components/Presets/index.tsx
@@ -1,5 +1,15 @@
+import { TabSelector } from "@/components/structural/components/TabSelector";
+import { Tables } from "./components/Tables";
+import { Items } from "./components/Items";
 import styles from "./index.module.css";
 
 export function Presets() {
-    return <div className={styles["presets"]}></div>;
+    return (
+        <TabSelector
+            tabs={{
+                tables: { name: "Tables", content: <Tables />, position: "left" },
+                items: { name: "Items", content: <Items />, position: "left" },
+            }}
+        />
+    );
 }
