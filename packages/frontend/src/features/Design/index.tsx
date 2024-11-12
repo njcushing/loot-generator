c9ee1fa6cf960ca647b0337cb05bd94e3808a767
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-12 21:43:34 +0000

    Delete redundant CSS file

--- packages/frontend/src/features/Design/index.tsx
+++ packages/frontend/src/features/Design/index.tsx
@@ -1,7 +1,6 @@
 import { TabSelector } from "@/components/structural/components/TabSelector";
 import { Interactive } from "./components/Interactive";
 import { JSONDisplay } from "./components/JSONDisplay";
-import styles from "./index.module.css";
 
 export function Design() {
     return (
