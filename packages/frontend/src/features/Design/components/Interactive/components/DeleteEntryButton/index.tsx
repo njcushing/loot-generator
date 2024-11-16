36c1529cb078b9aa493d18b0fbccffafb16bee77
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-16 12:45:34 +0000

    Write 'deletePresetEntry' function for DeleteEntryButton component

--- packages/frontend/src/features/Design/components/Interactive/components/DeleteEntryButton/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/components/DeleteEntryButton/index.tsx
@@ -1,6 +1,7 @@
 import { useContext, useCallback } from "react";
 import { LootGeneratorContext } from "@/pages/LootGenerator";
 import { LootItem, LootTable } from "@/utils/types";
+import { InteractiveContext } from "../..";
 import { deleteEntry } from "../../utils/manageEntries";
 import styles from "./index.module.css";
 
@@ -10,20 +11,31 @@ export type TDeleteEntryButton = {
 
 export function DeleteEntryButton({ entry }: TDeleteEntryButton) {
     const { lootGeneratorState, setLootGeneratorStateProperty } = useContext(LootGeneratorContext);
+    const { menuType } = useContext(InteractiveContext);
 
-    const deleteEntryAndSetNewTable = useCallback(() => {
+    const deleteActiveEntry = useCallback(() => {
         const { key } = entry;
         const copy: LootTable = JSON.parse(JSON.stringify(lootGeneratorState.lootTable));
-        deleteEntry(key, copy);
+        deleteEntry(key, copy.loot);
         setLootGeneratorStateProperty("lootTable", copy);
     }, [entry, lootGeneratorState.lootTable, setLootGeneratorStateProperty]);
+
+    const deletePresetEntry = useCallback(() => {
+        const { key } = entry;
+        const copy: (LootItem | LootTable)[] = JSON.parse(
+            JSON.stringify(lootGeneratorState.presets),
+        );
+        deleteEntry(key, copy);
+        setLootGeneratorStateProperty("presets", copy);
+    }, [entry, lootGeneratorState.presets, setLootGeneratorStateProperty]);
 
     return (
         <button
             type="button"
             className={`${styles["delete-entry-button"]} material-symbols-sharp`}
             onClick={(e) => {
-                deleteEntryAndSetNewTable();
+                if (menuType === "active") deleteActiveEntry();
+                if (menuType === "presets") deletePresetEntry();
                 e.currentTarget.blur();
             }}
             onMouseLeave={(e) => {
