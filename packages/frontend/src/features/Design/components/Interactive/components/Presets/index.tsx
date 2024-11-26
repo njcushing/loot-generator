b24e6d3da800252f7a61727f446a226a518fc5e2
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-26 20:11:55 +0000

    Add a separator between types of presets in Presets tab

--- packages/frontend/src/features/Design/components/Interactive/components/Presets/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/components/Presets/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { LootGeneratorContext } from "@/pages/LootGenerator";
 import { TableEntry } from "../TableEntry";
 import { ItemEntry } from "../ItemEntry";
@@ -7,17 +7,30 @@ import styles from "./index.module.css";
 export function Presets() {
     const { lootGeneratorState } = useContext(LootGeneratorContext);
 
+    const sortedPresets = useMemo(() => {
+        return lootGeneratorState.presets.sort((a, b) => b.type.localeCompare(a.type));
+    }, [lootGeneratorState.presets]);
+
     return (
         <ul className={styles["presets"]}>
-            {lootGeneratorState.presets
-                .sort((a, b) => b.type.localeCompare(a.type))
-                .map((preset) => {
-                    if (preset.type === "table")
-                        return <TableEntry entry={preset} isPreset key={preset.key} />;
-                    if (preset.type === "item")
-                        return <ItemEntry entry={preset} isPreset key={preset.key} />;
-                    return null;
-                })}
+            {sortedPresets.map((preset, i) => {
+                const elements: JSX.Element[] = [];
+                if (i > 0 && sortedPresets[i - 1].type !== sortedPresets[i].type) {
+                    elements.push(
+                        <div
+                            className={styles["separator"]}
+                            key={`${sortedPresets[i - 1].type}-${sortedPresets[i].type}-presets-separator`}
+                        ></div>,
+                    );
+                }
+                if (preset.type === "table") {
+                    elements.push(<TableEntry entry={preset} isPreset key={preset.key} />);
+                }
+                if (preset.type === "item") {
+                    elements.push(<ItemEntry entry={preset} isPreset key={preset.key} />);
+                }
+                return elements;
+            })}
         </ul>
     );
 }
