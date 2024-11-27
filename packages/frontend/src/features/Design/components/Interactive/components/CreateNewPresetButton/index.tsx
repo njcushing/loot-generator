4dbd21080ee5eeab83b2fade3069d18bf3ec9341
njcushing <129661299+njcushing@users.noreply.github.com>
2024-11-27 20:46:02 +0000

    Write style rules for CreateNewPresetButton component

--- packages/frontend/src/features/Design/components/Interactive/components/CreateNewPresetButton/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/components/CreateNewPresetButton/index.tsx
@@ -1,5 +1,18 @@
 import styles from "./index.module.css";
 
 export function CreateNewPresetButton() {
-    return <button type="button" className={styles["create-new-preset-button"]}></button>;
+    return (
+        <button
+            type="button"
+            className={`${styles["create-new-preset-button"]} material-symbols-sharp`}
+            onClick={(e) => {
+                e.currentTarget.blur();
+            }}
+            onMouseLeave={(e) => {
+                e.currentTarget.blur();
+            }}
+        >
+            Add
+        </button>
+    );
 }
