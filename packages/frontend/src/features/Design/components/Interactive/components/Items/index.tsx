9e96dfd13ca9ea74c0cc4e45d9546de6079ecb58
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-07 18:36:06 +0000

    Display Item component for each entry in 'items' state map

--- packages/frontend/src/features/Design/components/Interactive/components/Items/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/components/Items/index.tsx
@@ -1,9 +1,19 @@
+import { useContext } from "react";
+import { LootGeneratorContext } from "@/pages/LootGenerator";
+import { Item } from "../Item";
 import styles from "./index.module.css";
 
 export function Items() {
+    const { lootGeneratorState } = useContext(LootGeneratorContext);
+
     return (
         <div className={styles["items-tab"]}>
-            <ul className={styles["items"]}></ul>
+            <ul className={styles["items"]}>
+                {[...lootGeneratorState.items.keys()].map((key) => {
+                    const item = lootGeneratorState.items.get(key);
+                    return item && <Item key={key} />;
+                })}
+            </ul>
             <div className={styles["create-new-item-options"]}>
                 <button
                     type="button"
