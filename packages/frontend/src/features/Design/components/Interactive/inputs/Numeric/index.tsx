9eeedbf753a993a15ec0da448ae51a20679a98a7
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-10 23:32:56 +0000

    Ensure 'updateItem' function used when applicable

--- packages/frontend/src/features/Design/components/Interactive/inputs/Numeric/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/inputs/Numeric/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useCallback } from "react";
+import { useContext } from "react";
 import { LootGeneratorContext } from "@/pages/LootGenerator";
 import { LootItem, LootTable } from "@/utils/types";
 import { InteractiveContext } from "../..";
@@ -23,13 +23,8 @@ export function Numeric({
     fieldPath,
     disabled = false,
 }: TNumeric) {
-    const { mutateEntryField } = useContext(LootGeneratorContext);
+    const { updateItem, mutateEntryField } = useContext(LootGeneratorContext);
     const { menuType } = useContext(InteractiveContext);
-
-    const editEntryField = useCallback(
-        (newValue: unknown) => mutateEntryField(entryKey, [fieldPath], newValue, menuType),
-        [entryKey, fieldPath, mutateEntryField, menuType],
-    );
 
     return (
         <label
@@ -46,7 +41,10 @@ export function Numeric({
                     let newValue = Number(e.target.value);
                     if (min) newValue = Math.max(min, newValue);
                     if (max) newValue = Math.min(max, newValue);
-                    editEntryField(newValue);
+                    if (menuType === "items") updateItem(entryKey, [fieldPath], newValue);
+                    if (menuType === "active" || menuType === "presets") {
+                        mutateEntryField(entryKey, [fieldPath], newValue, menuType);
+                    }
                 }}
                 disabled={disabled}
             ></input>
--- packages/frontend/src/features/Design/components/Interactive/inputs/Text/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/inputs/Text/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useCallback } from "react";
+import { useContext } from "react";
 import { LootGeneratorContext } from "@/pages/LootGenerator";
 import { LootItem, LootTable } from "@/utils/types";
 import { InteractiveContext } from "../..";
@@ -13,13 +13,8 @@ export type TText = {
 };
 
 export function Text({ entryKey, labelText, value, fieldPath, disabled = false }: TText) {
-    const { mutateEntryField } = useContext(LootGeneratorContext);
+    const { updateItem, mutateEntryField } = useContext(LootGeneratorContext);
     const { menuType } = useContext(InteractiveContext);
-
-    const editEntryField = useCallback(
-        (newValue: unknown) => mutateEntryField(entryKey, [fieldPath], newValue, menuType),
-        [entryKey, fieldPath, mutateEntryField, menuType],
-    );
 
     return (
         <label className={styles["text-field-label"]} htmlFor={`${entryKey}-${fieldPath.join()}`}>
@@ -31,7 +26,10 @@ export function Text({ entryKey, labelText, value, fieldPath, disabled = false }
                 value={value}
                 onChange={(e) => {
                     const newValue = e.target.value;
-                    editEntryField(newValue);
+                    if (menuType === "items") updateItem(entryKey, [fieldPath], newValue);
+                    if (menuType === "active" || menuType === "presets") {
+                        mutateEntryField(entryKey, [fieldPath], newValue, menuType);
+                    }
                 }}
                 disabled={disabled}
             ></input>
