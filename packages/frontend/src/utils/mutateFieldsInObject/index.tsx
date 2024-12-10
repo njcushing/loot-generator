a7ca419b2f55421eac1d3c55638e8413cd0230be
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-10 23:46:28 +0000

    Write 'updateFieldsInObject' function

---
+++ packages/frontend/src/utils/mutateFieldsInObject/index.tsx
@@ -0,0 +1,22 @@
+export type TFieldToUpdate = {
+    path: string[];
+    newValue: unknown;
+};
+
+export const updateFieldsInObject = (obj: object, fieldsToUpdate: TFieldToUpdate[]): void => {
+    for (let i = 0; i < fieldsToUpdate.length; i++) {
+        const { path, newValue } = fieldsToUpdate[i];
+        let currentField = obj;
+        for (let j = 0; j < path.length; j++) {
+            const fieldName = path[j] as keyof typeof currentField;
+            const field = currentField[fieldName];
+            if (j === path.length - 1) {
+                (currentField[fieldName] as unknown) = newValue;
+                break;
+            }
+            if (typeof field === "object" && field !== null) {
+                (currentField as unknown) = field;
+            } else break;
+        }
+    }
+};
