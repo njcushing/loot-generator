b2852eff2152db961cf5b29b1652fc7acaae9869
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-11 00:46:59 +0000

    Add 'text' and 'style' props to Option component

--- packages/frontend/src/features/Design/components/Option/index.tsx
+++ packages/frontend/src/features/Design/components/Option/index.tsx
@@ -1,12 +1,31 @@
+import { CSSProperties, useMemo } from "react";
+import _ from "lodash";
 import styles from "./index.module.css";
 
 export type TOption = {
     symbol: string;
+    text?: string;
     onClick?: () => unknown;
     disabled?: boolean;
+    style?: {
+        width?: CSSProperties["width"];
+    };
 };
 
-export function Option({ symbol, onClick, disabled = false }: TOption) {
+const defaultStyle: Required<TOption["style"]> = {
+    width: "auto",
+};
+
+export function Option({ symbol, text, onClick, disabled = false, style }: TOption) {
+    const concatenatedStyle = useMemo(
+        () => _.merge(structuredClone(defaultStyle), style || {}),
+        [style],
+    );
+
+    const gridTemplateColumns: CSSProperties["gridTemplateColumns"] = useMemo(() => {
+        return text ? "auto minmax(0, 1fr)" : "auto";
+    }, [text]);
+
     return (
         <button
             type="button"
@@ -19,8 +38,14 @@ export function Option({ symbol, onClick, disabled = false }: TOption) {
                 e.currentTarget.blur();
             }}
             disabled={disabled}
+            style={{
+                gridTemplateColumns,
+
+                width: concatenatedStyle.width,
+            }}
         >
             <p className={`${styles["symbol"]} material-symbols-sharp`}>{symbol}</p>
+            {text && <p className="truncate-ellipsis">New Table</p>}
         </button>
     );
 }
