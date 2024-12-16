a0292bc7432f7bf7a517c176ad6b38d29ece7042
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-16 22:17:32 +0000

    Write functionality for SelectItem component

--- packages/frontend/src/features/Design/components/Interactive/components/SelectItem/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/components/SelectItem/index.tsx
@@ -1,5 +1,40 @@
-import styles from "./index.module.css";
+import { useMemo } from "react";
+import { ToggleBar, TToggleBar } from "@/components/buttons/components/ToggleBar";
 
-export function SelectItem() {
-    return <div></div>;
+export type TSelectItem = {
+    onClick?: () => unknown;
+};
+
+export function SelectItem({ onClick }: TSelectItem) {
+    const toggleBarOptions = useMemo((): TToggleBar["options"] => {
+        const options: TToggleBar["options"] = [];
+        options.push({
+            symbol: "Edit_Square",
+            onClick: () => {
+                if (onClick) onClick();
+            },
+        });
+        return options;
+    }, [onClick]);
+
+    return (
+        <ToggleBar
+            name="Please select an item"
+            defaultState={false}
+            options={toggleBarOptions}
+            onClick={() => {
+                if (onClick) onClick();
+            }}
+            style={{
+                size: "s",
+                colours: {
+                    normal: "rgb(181, 186, 255)",
+                    hover: "rgb(164, 169, 252)",
+                    focus: "rgb(155, 161, 252)",
+                },
+                indicator: "none",
+                nameFontStyle: "italic",
+            }}
+        />
+    );
 }
