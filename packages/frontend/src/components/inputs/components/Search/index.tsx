e2c75424015f976bc01211a67be14e9fac2c43ed
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-18 00:32:09 +0000

    Write basic functionality for Search component

--- packages/frontend/src/components/inputs/components/Search/index.tsx
+++ packages/frontend/src/components/inputs/components/Search/index.tsx
@@ -1,5 +1,19 @@
 import styles from "./index.module.css";
 
-export function Search() {
-    return <input type="search" />;
+export type TSearch = {
+    value?: number;
+    disabled?: boolean;
+    onChange?: (value: string) => unknown;
+};
+
+export function Search({ value, disabled, onChange }: TSearch) {
+    return (
+        <input
+            type="search"
+            className={styles["search-input"]}
+            defaultValue={value}
+            onChange={(e) => onChange && onChange(e.currentTarget.value)}
+            disabled={disabled}
+        />
+    );
 }
