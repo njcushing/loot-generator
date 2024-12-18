a090f9958d91ee454a9ec5224da060fd194fae32
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-18 00:52:48 +0000

    Filter items in Items tab based on current search bar value

--- packages/frontend/src/features/Design/components/Interactive/components/Items/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/components/Items/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext } from "react";
+import { useContext, useState, useCallback } from "react";
 import { LootGeneratorContext } from "@/pages/LootGenerator";
 import { Option } from "@/features/Design/components/Option";
 import { Inputs } from "@/components/inputs";
@@ -9,6 +9,9 @@ import styles from "./index.module.css";
 export function Items() {
     const { lootGeneratorState, addNewItem } = useContext(LootGeneratorContext);
     const { setMenuStates } = useContext(InteractiveContext);
+
+    const [currentSearch, setCurrentSearch] = useState<string>("");
+    const filterRegex = new RegExp(`${currentSearch}`, "i");
 
     const expandItems = useCallback(() => {
         setMenuStates(new Map(lootGeneratorState.items.keys().map((key) => [key, "expanded"])));
@@ -21,15 +24,24 @@ export function Items() {
     return (
         <div className={styles["items-tab"]}>
             <div className={styles["items-tab-options"]}>
-                <Inputs.Search />
+                <Inputs.Search
+                    value={currentSearch}
+                    onChange={(value) => setCurrentSearch(value)}
+                />
                 <Option symbol="Expand_Content" onClick={() => expandItems()} />
                 <Option symbol="Collapse_Content" onClick={() => collapseItems()} />
             </div>
             <ul className={styles["items"]}>
-                {[...lootGeneratorState.items.keys()].map((key) => {
-                    const item = lootGeneratorState.items.get(key);
-                    return item && <Item id={key} key={key} />;
-                })}
+                {[...lootGeneratorState.items.keys()]
+                    .filter((key) => {
+                        const item = lootGeneratorState.items.get(key);
+                        if (!item) return false;
+                        return filterRegex.test(item.name || "");
+                    })
+                    .map((key) => {
+                        const item = lootGeneratorState.items.get(key);
+                        return item && <Item id={key} key={key} />;
+                    })}
             </ul>
             <div className={styles["create-new-item-options"]}>
                 <button
