c462ae01fcc606b01d97aef6384b1a07fd1873ba
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-22 13:41:05 +0000

    Write basic functionality and display name input for Table component

--- packages/frontend/src/features/Design/components/Interactive/components/Table/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/components/Table/index.tsx
@@ -1,5 +1,61 @@
+import { useContext, useMemo } from "react";
+import { LootGeneratorContext } from "@/pages/LootGenerator";
+import { ToggleBar, TToggleBar } from "@/components/buttons/components/ToggleBar";
+import { Inputs } from "../../inputs";
 import styles from "./index.module.css";
 
-export function Table() {
-    return <div></div>;
+export type TTable = {
+    id: string;
+    onClick?: () => unknown;
+};
+
+export function Table({ id, onClick }: TTable) {
+    const { lootGeneratorState, deleteTable } = useContext(LootGeneratorContext);
+
+    const table = useMemo(() => lootGeneratorState.tables.get(id), [id, lootGeneratorState.tables]);
+
+    const toggleBarOptions = useMemo((): TToggleBar["options"] => {
+        const options: TToggleBar["options"] = [];
+        options.push({
+            symbol: "Delete",
+            onClick: () => {
+                deleteTable(id);
+            },
+            colours: { hover: "rgb(255, 120, 120)", focus: "rgb(255, 83, 83)" },
+        });
+        return options;
+    }, [id, deleteTable]);
+
+    if (!table) return null;
+
+    const { name } = table;
+    const displayName = name || "Unnamed Table";
+
+    const colours = {
+        normal: "rgb(186, 240, 228)",
+        hover: "rgb(157, 224, 210)",
+        focus: "rgb(139, 206, 191)",
+    };
+
+    return (
+        <ToggleBar
+            name={displayName}
+            options={toggleBarOptions}
+            onClick={() => onClick && onClick()}
+            style={{
+                colours,
+                nameFontStyle: name ? "normal" : "italic",
+            }}
+            key={id}
+        >
+            <div className={styles["table-fields"]}>
+                <Inputs.Text
+                    entryKey={id}
+                    labelText="Name"
+                    value={name || ""}
+                    fieldPath={["name"]}
+                />
+            </div>
+        </ToggleBar>
+    );
 }
