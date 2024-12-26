8a97f5d5400f1ee43321620ae2f11019cfea5cdb
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-26 11:20:46 +0000

    Add search bar to Tables tab

--- packages/frontend/src/features/Design/components/Interactive/components/Tables/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/components/Tables/index.tsx
@@ -1,11 +1,15 @@
-import { useContext, useMemo } from "react";
+import { useContext, useState, useMemo } from "react";
 import { LootGeneratorContext } from "@/pages/LootGenerator";
 import { Option } from "@/features/Design/components/Option";
+import { Inputs } from "@/components/inputs";
 import { Table } from "../Table";
 import styles from "./index.module.css";
 
 export function Tables() {
     const { lootGeneratorState, addNewTable } = useContext(LootGeneratorContext);
+
+    const [currentSearch, setCurrentSearch] = useState<string>("");
+    const filterRegex = new RegExp(`${currentSearch}`, "i");
 
     const tableKeys = useMemo(() => {
         return [...lootGeneratorState.tables.keys()];
@@ -13,12 +17,24 @@ export function Tables() {
 
     return (
         <div className={styles["tables-tab"]}>
+            <div className={styles["tables-tab-options"]}>
+                <Inputs.Search
+                    value={currentSearch}
+                    onChange={(value) => setCurrentSearch(value)}
+                />
+            </div>
             <ul className={styles["tables"]}>
                 {tableKeys.length > 0 ? (
-                    tableKeys.map((key) => {
-                        const table = lootGeneratorState.tables.get(key);
-                        return table && <Table id={key} key={key} />;
-                    })
+                    tableKeys
+                        .filter((key) => {
+                            const table = lootGeneratorState.tables.get(key);
+                            if (!table) return false;
+                            return filterRegex.test(table.name || "");
+                        })
+                        .map((key) => {
+                            const table = lootGeneratorState.tables.get(key);
+                            return table && <Table id={key} key={key} />;
+                        })
                 ) : (
                     <p
                         className={styles["help-message"]}
