4e1da7395dbff4494cdfd42836bd8149844d17f9
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-27 13:05:16 +0000

    Display ToggleBar within Entry component

--- packages/frontend/src/features/Design/components/Interactive/components/Entry/index.tsx
+++ packages/frontend/src/features/Design/components/Interactive/components/Entry/index.tsx
@@ -1,5 +1,22 @@
+import { ToggleBar } from "@/components/buttons/components/ToggleBar";
+import { v4 as uuid } from "uuid";
 import styles from "./index.module.css";
 
 export function Entry() {
-    return <li className={styles["entry"]}></li>;
+    return (
+        <li className={styles["entry"]} key={uuid()}>
+            <ToggleBar
+                name="New Entry"
+                style={{
+                    size: "s",
+                    colours: {
+                        normal: "rgb(253, 222, 47)",
+                        hover: "rgb(240, 208, 31)",
+                        focus: "rgb(218, 187, 14)",
+                    },
+                    nameFontStyle: "italic",
+                }}
+            ></ToggleBar>
+        </li>
+    );
 }
