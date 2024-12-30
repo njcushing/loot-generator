379c31efa1246d064739c136f9975a00879acd8f
njcushing <129661299+njcushing@users.noreply.github.com>
2024-12-30 22:57:55 +0000

    Update functions to allow setting ids on 'noid' entries

--- packages/frontend/src/pages/LootGenerator/index.tsx
+++ packages/frontend/src/pages/LootGenerator/index.tsx
@@ -306,16 +306,13 @@ export function LootGenerator() {
             if (!result) return false;
             const { entry, copy } = result;
 
-            if (entry.type === "table_id") entry.id = setTableId;
-            if (entry.type === "entry" || entry.type === "item_id") {
-                const table = createLootTable("table_id", {
-                    ...entry,
-                    type: "table_id",
-                    id: setTableId,
-                });
-                Object.keys(entry).forEach((key) => delete entry[key as keyof typeof entry]);
-                Object.assign(entry, table);
-            }
+            const table = createLootTable("table_id", {
+                ...entry,
+                type: "table_id",
+                id: setTableId,
+            });
+            Object.keys(entry).forEach((key) => delete entry[key as keyof typeof entry]);
+            Object.assign(entry, table);
 
             saveCopy("tables", copy);
 
@@ -332,16 +329,13 @@ export function LootGenerator() {
             if (!result) return false;
             const { entry, copy } = result;
 
-            if (entry.type === "item_id") entry.id = setItemId;
-            if (entry.type === "entry" || entry.type === "table_id") {
-                const item = createLootItem("item_id", {
-                    ...entry,
-                    type: "item_id",
-                    id: setItemId,
-                });
-                Object.keys(entry).forEach((key) => delete entry[key as keyof typeof entry]);
-                Object.assign(entry, item);
-            }
+            const item = createLootItem("item_id", {
+                ...entry,
+                type: "item_id",
+                id: setItemId,
+            });
+            Object.keys(entry).forEach((key) => delete entry[key as keyof typeof entry]);
+            Object.assign(entry, item);
 
             saveCopy("tables", copy);
 
