b60070e9558380da552f596242c79fab8ec568c3
njcushing <129661299+njcushing@users.noreply.github.com>
2025-01-08 19:03:15 +0000

    Add 'text' prop to PopUpModal and display it within component

--- packages/frontend/src/components/ui/components/PopUpModal/index.tsx
+++ packages/frontend/src/components/ui/components/PopUpModal/index.tsx
@@ -1,5 +1,13 @@
 import styles from "./index.module.css";
 
-export function PopUpModal() {
-    return <div className={styles["pop-up-modal"]}></div>;
+export type TPopUpModal = {
+    text: string;
+};
+
+export function PopUpModal({ text }: TPopUpModal) {
+    return (
+        <div className={styles["pop-up-modal"]}>
+            <p className={styles["text"]}>{text}</p>
+        </div>
+    );
 }
