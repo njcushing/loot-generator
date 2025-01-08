52d0c1b1280b3a657a1d46a0f56752b04992e77a
njcushing <129661299+njcushing@users.noreply.github.com>
2025-01-08 20:07:32 +0000

    Add 'timer' prop to PopUpModal component

--- packages/frontend/src/components/ui/components/PopUpModal/index.tsx
+++ packages/frontend/src/components/ui/components/PopUpModal/index.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useRef, useMemo } from "react";
 import styles from "./index.module.css";
 
 export type TPopUpModal = {
     text: string;
+    timer?: {
+        duration?: number;
+        callback?: () => unknown;
+    };
 };
 
-export function PopUpModal({ text }: TPopUpModal) {
+const defaultTimer: Required<TPopUpModal["timer"]> = {
+    duration: -1,
+    callback: () => {},
+};
+
+export function PopUpModal({ text, timer }: TPopUpModal) {
+    const concatenatedTimer = useMemo(() => {
+        return { ...defaultTimer, ...timer };
+    }, [timer]);
+
+    const timerRef = useRef<NodeJS.Timeout>();
+    useEffect(() => {
+        const { duration, callback } = concatenatedTimer!;
+
+        if (Number.isNaN(timerRef.current)) clearTimeout(timerRef.current);
+
+        timerRef.current = setTimeout(() => callback && callback(), duration);
+
+        return () => {
+            if (Number.isNaN(timerRef.current)) clearTimeout(timerRef.current);
+        };
+    }, [concatenatedTimer]);
+
     return (
         <div className={styles["pop-up-modal"]}>
             <p className={styles["text"]}>{text}</p>
