b040b218a18b0a5d763b859a44e4b4c2e78071ea
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-02 16:40:26 +0000

    Write findCompatibleDescendantTables function
    This function scans all tables in the LootGenerator component's state, and finds which of them can be set as a descendant table of the specified table. If a table is an ancestor of the specified table at any point, it cannot be set as a descendant table, otherwise the possibility of an infinite recursion arises when generating loot from the table.

---
+++ packages/frontend/src/utils/findCompatibleDescendantTables/index.ts
@@ -0,0 +1,38 @@
+import { LootGeneratorState } from "@/pages/LootGenerator";
+import { Table } from "../types";
+
+export const findCompatibleDescendantTables = (
+    target: string,
+    tables: LootGeneratorState["tables"],
+): Set<string> => {
+    let incompatibleTables: Set<string> = new Set([target]);
+
+    const scanLootTable = (ancestors: Set<string>, loot: Table["loot"]) => {
+        for (let i = 0; i < loot.length; i++) {
+            const entry = loot[i];
+            const { type } = entry;
+            if (type === "table_id") {
+                const { id } = entry;
+                if (!id) return;
+                if (id === target) {
+                    incompatibleTables = new Set([...incompatibleTables, ...ancestors]);
+                }
+                const table = tables[id];
+                scanLootTable(new Set([...ancestors, id]), table.loot);
+            }
+            if (type === "table_noid") {
+                const { loot: entryLoot } = entry;
+                scanLootTable(ancestors, entryLoot);
+            }
+        }
+    };
+
+    [...Object.entries(tables)].forEach((table) => {
+        const [key, value] = table;
+        if (!incompatibleTables.has(key)) scanLootTable(new Set([key]), value.loot);
+    });
+
+    const compatibleTables = new Set([...Object.keys(tables)]).difference(incompatibleTables);
+
+    return compatibleTables;
+};
