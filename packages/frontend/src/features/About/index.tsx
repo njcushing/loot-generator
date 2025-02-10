2f424f816d29bda9e881680708741b5bc3997b62
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-10 21:34:53 +0000

    Create sections for About component

--- packages/frontend/src/features/About/index.tsx
+++ packages/frontend/src/features/About/index.tsx
@@ -1,5 +1,49 @@
+import { Structural } from "@/components/structural/";
 import styles from "./index.module.css";
 
 export function About() {
-    return <div className={styles["about"]}></div>;
+    return (
+        <div className={styles["about"]}>
+            <Structural.TabSelector
+                tabs={{
+                    overview: {
+                        name: "Overview",
+                        content: (
+                            <section className={styles["section-container"]}>
+                                <h2 className={styles["title"]}>Overview</h2>
+                            </section>
+                        ),
+                        position: "left",
+                    },
+                    items: {
+                        name: "Items",
+                        content: (
+                            <section className={styles["section-container"]}>
+                                <h2 className={styles["title"]}>Items</h2>
+                            </section>
+                        ),
+                        position: "left",
+                    },
+                    tables: {
+                        name: "Tables",
+                        content: (
+                            <section className={styles["section-container"]}>
+                                <h2 className={styles["title"]}>Tables</h2>
+                            </section>
+                        ),
+                        position: "left",
+                    },
+                    JSON: {
+                        name: "JSON",
+                        content: (
+                            <section className={styles["section-container"]}>
+                                <h2 className={styles["title"]}>JSON</h2>
+                            </section>
+                        ),
+                        position: "left",
+                    },
+                }}
+            />
+        </div>
+    );
 }
